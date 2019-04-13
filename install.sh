d4c74d8d822c31b113310a522932a41a492f6e2a
Nicholas O'Brien <ndo9903@rit.edu>
2019-04-13 15:01:59 -0400

    updated to nginx 1.15

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ apt install -y libpcre3 libpcre3-dev zlib1g zlib1g-dev openssl libssl-dev
 # yum install -y pcre pcre-devel zlib zlib-devel openssl openssl-devel
 
 # pull down nginx
-wget https://nginx.org/download/nginx-1.14.0.tar.gz
-tar zxf nginx-1.14.0.tar.gz
-cd nginx-1.14.0
+wget https://nginx.org/download/nginx-1.15.0.tar.gz
+tar zxf nginx-1.15.0.tar.gz
+cd nginx-1.15.0
 
 # add our module as a dependency
 ./configure --add-module=../
